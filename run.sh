d206315acbe62c6a08e157771f33ca55f4d4773c
Jeverson Dias da Silva <jeversondiasdasilva@gmail.com>
2025-11-24 12:36:01 -0300

    Create custom initialization script and setup function
    Added a user customization script and initial configurations.

--- run.sh
+++ run.sh
@@ -2,6 +2,19 @@
 # Curitiba 24 de Novembro de 2025.
 # Editor: Jeverson D. Silva   ///@JCGAMESCLASSICOS...
 
+# Criando gatilho para comandos de inicialização com as personalizações do usuário.
+cat << 'EOF' > /userdata/system/custom.sh
+#!/bin/bash
+# Curitiba 24 de Novembro de 2025.
+# Editor: Jeverson D Silva   ///@JCGAMESCLASSICOS...
+
+# Comandos a serem carregados juntamente com o sistema.
+
+EOF
+
+
+
+# Funções
 configs_iniciais() {
     
     # Configurações de idioma e fuso horário
@@ -12,8 +25,14 @@ configs_iniciais() {
     if grep -q '#system.timezone=Europe/Paris' /userdata/system/batocera.conf; then
         sed -i 's|#system.timezone=Europe/Paris|system.timezone=America/Sao_Paulo|' /userdata/system/batocera.conf
     fi
-
-    # Config teclado
+    if [ ! -f /usr/bin/wmctrl ]; then
+        wget https://github.com/JeversonDiasSilva/configs/releases/download/v.1.0/wmctrl -O /usr/bin/wmctrl
+        chmod +x /usr/bin/wmctrl
+    fi
+    if [ ! -f /usr/bin/xdotool ]; then
+        wget https://github.com/JeversonDiasSilva/configs/releases/download/v.1.0/xdotool -O /usr/bin/xdotool
+        chmod +x /usr/bin/xdotool
+    fi
 
 }
 configs_iniciais
