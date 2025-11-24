166753734573ca17dd2eaebd334b433701212a01
Jeverson Dias da Silva <jeversondiasdasilva@gmail.com>
2025-11-24 15:52:18 +0100

    Refactor initial configuration function and comments
    Commit inicial.

--- run.sh
+++ run.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-# Curitiba 24 de Novembro de 2025
-# Editor Jeverson D. Silva   ///@JCGAMESCLASSICOS...
+# Curitiba 24 de Novembro de 2025.
+# Editor: Jeverson D. Silva   ///@JCGAMESCLASSICOS...
 
-# Configurações iniciais do  sistema.
-configs_sistema() {
+configs_iniciais() {
+    
+    # Configurações de idioma e fuso horário
+    if grep -q '^#system.language=en_US' /userdata/system/batocera.conf; then
+        sed -i 's|#system.language=en_US|system.language=pt_BR|' /userdata/system/batocera.conf
+    fi
 
-}  
-configs_sistema
+    if grep -q '#system.timezone=Europe/Paris' /userdata/system/batocera.conf; then
+        sed -i 's|#system.timezone=Europe/Paris|system.timezone=America/Sao_Paulo|' /userdata/system/batocera.conf
+    fi
+
+    # Config teclado
+
+}
+configs_iniciais
+
