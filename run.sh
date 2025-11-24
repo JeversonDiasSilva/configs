cda7b7c7158dc7b1943ac515d07e9a548dd70d80
Jeverson Dias da Silva <jeversondiasdasilva@gmail.com>
2025-11-24 08:25:16 -0300

    Create run.sh with initial system configuration setup
    Added initial script setup with a function for system configurations.

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Curitiba 24 de Novembro de 2025
+# Editor Jeverson D. Silva   ///@JCGAMESCLASSICOS...
+
+# Configurações iniciais do  sistema.
+configs_sistema() {
+
+}  
+configs_sistema
